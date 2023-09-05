c693ff458f0285e42bb8947fea11fb6368f7a9d3
unknown <modiayush@iitgn.ac.in>
2023-09-05 16:30:13 +0530

    changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -126,7 +126,7 @@ const Work = () => {
                                 Portfolio Website
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href='/'>
+                                <a href='https://github.com/ayushmodi12/portfolio-website'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                                         Code
                                     </button>
